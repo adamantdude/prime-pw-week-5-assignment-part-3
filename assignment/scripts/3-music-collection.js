fa7b9062e0c4cb886bda836fde06039beeaec29d
Adam Lee <adam.plee23@gmail.com>
2022-09-14 16:53:56 -0500

    stretch goal revision

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,7 +3,7 @@ function addToCollection(title, artist, yearPublished, tracks) {
         title: title,           //string
         artist: artist,         //string
         year: yearPublished,    //integer
-        tracks: tracks          //2d array; [0] names, [1] durations
+        tracks: tracks          //object array, name:duration
     };
     collection.push(song);
     return song;
@@ -14,8 +14,8 @@ function showCollection(array) {
     // for every object in object-array, output the objects in set format
     for(const item of array) {
         console.log(item.title + ' by ' + item.artist + ', published in ' + item.year + ":");
-        // for loop. i index, to 2d array length. output all tracks
-        for(let i=0; i<item.tracks[0].length; ++i) console.log(i+1 + ". " + item.tracks[0][i] + ": " + item.tracks[1][i]);
+        // for loop. i index, to array length. output all tracks
+        for(let i=0; i<item.tracks.length; ++i) console.log(i+1 + ". ", item.tracks[i]);
     }
     return;
 }
@@ -32,9 +32,27 @@ function findByArtist(search) {
 function search(searchObject) {
     // !'' = true and !null = true, therefore if either searchObject element is empty, return the collection
     // implement early exit if possible
-    if(searchObject == null || !searchObject.artist || !searchObject.year) return collection;
+    if(!searchObject || !searchObject.artist || !searchObject.year) return collection;
     let results = [];
-    for(const item of collection) if(item.artist.indexOf(searchObject.artist) >= 0 && (item.year === searchObject.year)) results.push(item);
+    if(!searchObject.searchTrack) {
+        for(const item of collection) {
+            if(item.artist.toUpperCase().indexOf(searchObject.artist.toUpperCase()) >= 0 && (item.year === searchObject.year))
+                results.push(item);
+        }
+    }
+    else {
+        for(const item of collection) {
+            if(item.artist.toUpperCase().indexOf(searchObject.artist.toUpperCase()) >= 0 && (item.year === searchObject.year))
+                for(let i=0; i<item.tracks.length; ++i) 
+                    if(Object.keys(item.tracks[i])[0].toUpperCase() === searchObject.searchTrack.toUpperCase())
+                        results.push({
+                            title:item.title,
+                            artist:item.artist,
+                            year:item.year,
+                            tracks:item.tracks[i]
+                        });
+        }
+    }
     return results;
 }
 
@@ -43,16 +61,12 @@ function search(searchObject) {
 console.log('***** Music Collection *****')
 let collection = [];
 
-// wasn't sure how to implement an object array within an object. opted for simple 2d array
-// since it's easier to keep track of
-// [0][x] contains names, [1][x] contains durations
-
-console.log(addToCollection('Myth&Roid Overlord TV OST', 'MYTH & ROID' , 2018, [['Hydra', 'VORACITY'],['4:54', '3:51']]));
-console.log(addToCollection('Belle OST', 'Ludvig Forssell and Taisei Iwasaki', 2021, [['Gales of Song', 'A Million Miles Away'],['4:00', '8:02']]));
-console.log(addToCollection('Unorthodox Jukebox', 'Bruno Mars', 2012, [['Locked out of Heaven', 'When I Was Your Man'],['3:54', '3:32']]));
-console.log(addToCollection('The Script', 'The Script', 2008, [["The Man Who Can't Be Moved", "I'm Yours"],['4:02', '4:15']]));
-console.log(addToCollection('SHUT UP, GET HAPPY', 'Demondice', 2022, [['wanting, getting, wanting', 'take the bait', 'dark hour', 'fake ass gold'], ['2:33', '3:46', '4:12', '3:31']]));
-console.log(addToCollection('The Greatest Showman: Reimagined', 'Benj Pasek and Justin Paul', 2017, [['A Million Dreams', 'Rewrite The Stars', 'This Is Me'], ['4:34', '3:38', '4:26']]));
+console.log(addToCollection('Myth&Roid Overlord TV OST', 'MYTH & ROID' , 2018, [{'Hydra':'4:54'}, {'VORACITY':'3:51'}]));
+console.log(addToCollection('Belle OST', 'Ludvig Forssell and Taisei Iwasaki', 2021, [{'Gales of Song':'4:00'},{'A Million Miles Away':'8:02'}]));
+console.log(addToCollection('Unorthodox Jukebox', 'Bruno Mars', 2012, [{'Locked out of Heaven':'3:54'},{'When I Was Your Man':'3:32'}]));
+console.log(addToCollection('The Script', 'The Script', 2008, [{"The Man Who Can't Be Moved":"4:02"},{"I'm Yours":'4:15'}]));
+console.log(addToCollection('SHUT UP, GET HAPPY', 'Demondice', 2022, [{'wanting, getting, wanting':'2:33'},{'take the bait':'3:46'},{'dark hour':'4:12'},{'fake ass gold':'3:31'}]));
+console.log(addToCollection('The Greatest Showman: Reimagined', 'Benj Pasek and Justin Paul', 2017, [{'A Million Dreams':'4:34'},{'Rewrite The Stars':'3:38'}, {'This Is Me':'4:26'}]));
 console.log(collection);
 
 showCollection(collection);
@@ -74,7 +88,8 @@ showCollection(findByArtist('and'));
 
 let newSearch = {
     artist: "MYTH & ROID",
-    year: 2018
+    year: 2018,
+    searchTrack: "Hydra"
 };
 
 console.log('search() test with newSearch object');
@@ -82,20 +97,41 @@ console.log(search(newSearch));         // should return with one object array
 
 let newSearchTwo = {
     artist: 'Ray Charles',
-    year: 1957
+    year: 1957,
+    searchTrack: null
 };
 
 console.log('search() test with newSearchTwo object');
 console.log(search(newSearchTwo));      // should return empty array
 
+let newSearchThree = {
+    artist: "MYTH & ROID",
+    year: 2018,
+    searchTrack: 'VORACITY'
+};
+
+console.log('search() test with newSearchThree');
+console.log(search(newSearchThree));
+
+let newSearchFour = {
+    artist: "Demondice",
+    year: 2022,
+    searchTrack: "fake ass gold"
+};
+
+console.log('search() test with newSearchFour');
+console.log(search(newSearchFour));
+
 let emptySearch = {
     artist: '',             // empty
-    year: 2005
+    year: 2005,
+    searchTrack: null
 };
 
 let emptySearchTwo = {
     artist: 'Ed Sheeran',
-    year: null              // empty
+    year: null,             // empty
+    searchTrack: null
 };
 
 let emptySearchThree;       // empty
